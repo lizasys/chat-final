d5a613cf6537ad3dab92aa97666916962f32b5b1
martial <alain.mbogning@edu.unifi.it>
2024-05-22 21:51:53 +0200

    First program of Progetto_Labo C++ prima unit testing

---
+++ cmake-build-debug/Test_Progetto/lib/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Admin/CLionProjects/Progetto_Labo/Test_Progetto/lib/googletest/googlemock
+# Build directory: C:/Users/Admin/CLionProjects/Progetto_Labo/cmake-build-debug/Test_Progetto/lib/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
